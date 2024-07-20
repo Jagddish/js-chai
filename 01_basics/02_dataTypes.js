5b53083f4905cd0ac779404ab123362d1531d653
Jagddish <jagddish2@gmail.com>
2024-07-20 17:30:54 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as  newer version
+
+// alert(3 + 3); // we are using nodejs not browser
+
+console.log(3 + 3);
+
+console.log("Jagddish");
+
+let name = "jagddish";
+let age = 19;
+let isLoggedIn = false;
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean > true or false
+// null = standalone value | representation of empty value
+// undefined = variable is declared but not assigned any value yet
+// symbol = unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
