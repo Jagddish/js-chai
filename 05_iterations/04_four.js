fef48178df2ddca2bd0807f6e40dc2bb7c27368b
Jagddish <jagddish2@gmail.com>
2024-07-21 15:30:33 +0530

    for-in loop

--- 05_iterations/04_four.js
+++ 05_iterations/04_four.js
@@ -5,5 +5,25 @@ const myObj = {
 };
 
 for (const key in myObj) {
-  console.log(key);
+  // console.log(`${key} shortcut is for ${myObj[key]}`);
 }
+
+const arr = ["js", "ruby", "python", "c++"];
+
+for (const k in arr) {
+  // console.log(k); // prints index
+  // console.log(arr[k]);
+}
+
+// for array for-of is better
+for (const i of arr) {
+}
+// const map = new Map();
+// map.set("IN", "India");
+// map.set("UK", "Uninted Kingdom");
+// map.set("Fr", "France");
+// map.set("IN", "India");
+//
+// for (constkey in map) {
+//   console.log(key);
+// }
