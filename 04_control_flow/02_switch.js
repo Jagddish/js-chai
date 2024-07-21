334f91f538b1dec154475d1458572bcb8209ff3c
Jagddish <jagddish2@gmail.com>
2024-07-21 11:30:03 +0530

    switch

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//   case value:
+//
+//     break;
+//
+//   default:
+//     break;
+// }
+
+const month = 3;
+switch (month) {
+  case 1:
+    console.log("January");
+
+    break;
+  case 2:
+    console.log("Feb");
+
+    break;
+  case 3:
+    console.log("Mar");
+
+    break;
+  case 2:
+    console.log("Apr");
+
+    break;
+  case 2:
+    console.log("May");
+
+    break;
+
+  default:
+    console.log("Mar");
+    break;
+}
