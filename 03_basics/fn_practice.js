08db784c42f2342613e6c9c968e84981ac7e393c
Jagddish <jagddish2@gmail.com>
2024-07-23 22:45:42 +0530

    functions practice

---
+++ 03_basics/fn_practice.js
@@ -0,0 +1,23 @@
+// syntax
+// function fn-name(params) {
+// scope of the fn code block
+//}
+
+function hello() {
+  console.log("Hello world");
+}
+// execute function or calling
+hello();
+
+// function addTwoNumbers(num1, num2) {
+//   console.log(num1 + num2);
+// }
+//
+// const result = addTwoNumbers(1, 2);
+// console.log(result);
+
+function addTwoNumbers(num1, num2) {
+  console.log("addition");
+  return num1 + num2;
+}
+const result = addTwoNumbers(1, 2);
